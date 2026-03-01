d8318eca9d3e57b315e7856424844b4b1bd88d2a
Arya Shedbal <moony.shady.o7@gmail.com>
2026-03-02 01:39:01 +0530

    Updated SingleCycleCPU to include branch and jump logic
    Enhanced Single Cycle CPU design with support for jump and branch instructions.

--- Single Cycle Non Pipelined RISC V Core/SingleCycleCPU.v
+++ Single Cycle Non Pipelined RISC V Core/SingleCycleCPU.v
@@ -36,6 +36,8 @@ wire [1:0] ALUOp;
 wire memWrite;
 wire ALUSrc;
 wire regWrite;
+wire jal;
+wire jalr;
 Control m_Control( // extracting opcode from instruction and generating control signals accordingly
     .opcode(instruction[6:0]), 
     .branch(branch),
@@ -44,7 +46,15 @@ Control m_Control( // extracting opcode from instruction and generating control 
     .ALUOp(ALUOp),
     .memWrite(memWrite),
     .ALUSrc(ALUSrc),
-    .regWrite(regWrite)
+    .regWrite(regWrite),
+    .jal(jal),
+    .jalr(jalr)
+);
+
+wire [2:0] branchcontrol;
+branchcontroller m_branchcontroller (
+    .funct3(instruction[14:12]),
+    .branchcontrol(branchcontrol)
 );
 
 wire [31:0] rs2content;
@@ -69,23 +79,40 @@ ImmGen #(.Width(32)) m_ImmGen(
 );
 
 wire [31:0] immx2; 
-ShiftLeftOne m_ShiftLeftOne( // for beq instruction
+ShiftLeftOne m_ShiftLeftOne( // for branch and jump instruction
     .i(immval),
     .o(immx2)
 );
 
 wire [31:0] pcjumploc; 
-Adder m_Adder_2( // after beq where to jump
+Adder m_Adder_2( // after branch where to jump
     .a(pcout),
     .b(immx2),
     .sum(pcjumploc)
 );
 
+wire [31:0] ALUOut;
 wire zeroflag;
-Mux2to1 #(.size(32)) m_Mux_PC( // decides next pc
-    .sel(branch & zeroflag),
+Mux2to1 #(.size(32)) m_Mux_ZorALUOut( // in case of branch, decides whether zeroflag must be checked (beq, bne) or SLT output from ALU should be checked (blt, bge)
+    .sel(branchcontrol[1]),
+    .s0(zeroflag),
+    .s1(ALUOut),
+    .out(ZorALUOut)
+);
+
+wire [31:0] pcbasednextpc;
+Mux2to1 #(.size(32)) m_Mux_PC1( // decides next pc in case of not jalr
+    .sel((branch & (branchcontrol[0]^ZorALUOut)) | jal), // if (branch inst AND related condition) OR jal, select pcbasednextpc, else pc+4
     .s0(pcplus4),
     .s1(pcjumploc),
+    .out(pcbasednextpc)
+); // explanation of condition: for beq, blt take zeroflag / ALUOut as it is (branchcontrol[0]=0), 
+   // for bne, bge, complement zeroflag / ALUOut, since ALU does subtraction for beq, bne and SLT for blt, bge
+
+Mux2to1 #(.size(32)) m_Mux_PC( // decides next pc
+    .sel(jalr), // if jalr, select ALUOut which is rs1content+immval, else select previous mux output
+    .s0(pcbasednextpc),
+    .s1(ALUOut),
     .out(nextpc)
 );
 
@@ -105,7 +132,6 @@ ALUCtrl m_ALUCtrl( // controls ALU according to funct3 and funct7
     .ALUCtl(ALUCtl)
 );
 
-wire [31:0] ALUOut;
 ALU m_ALU(   // ALU
     .ALUCtl(ALUCtl),
     .A(rs1content),
@@ -125,10 +151,18 @@ DataMemory m_DataMemory( // to read from and write to memory
     .readData(datafrommem)
 );
 
-Mux2to1 #(.size(32)) m_Mux_WriteData( // to select whether rd should be written with ALU result or from memory
+wire [31:0] regcontent;
+Mux2to1 #(.size(32)) m_Mux_WriteData( // to select what should be written to reg unit: ALU result or from memory
     .sel(memtoReg),
     .s0(ALUOut),
     .s1(datafrommem),
+    .out(regcontent)
+);
+
+Mux2to1 #(.size(32)) m_Mux_RegContent( // to select whether rd should be written with above result or pcplus4 (in case of jalr)
+    .sel(jal | jalr),
+    .s0(regcontent),
+    .s1(pcplus4),
     .out(rdcontent)
 );
 
