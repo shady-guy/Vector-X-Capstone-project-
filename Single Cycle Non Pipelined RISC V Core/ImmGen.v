7f806eeabf8be705382acc34588fde68e25aba68
Arya Shedbal <moony.shady.o7@gmail.com>
2026-03-02 01:38:20 +0530

    Add support for jalr and jal instructions in ImmGen

--- Single Cycle Non Pipelined RISC V Core/ImmGen.v
+++ Single Cycle Non Pipelined RISC V Core/ImmGen.v
@@ -18,12 +18,18 @@ module ImmGen#(parameter Width = 32) (
             7'b0100011: begin // sw : S
                 imm = {{20{inst[31]}}, inst[31:25], inst[11:7]};
             end
-            7'b1100011: begin // beq : SB
+            7'b1100011: begin // branch : SB
                 imm = {{20{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8]};
+            end
+            7'b1100111: begin // jalr : I 
+                imm = {{20{inst[31]}}, inst[31:20]};
+            end
+            7'b1101111: begin // jal : UJ 
+                imm = {{12{inst[31]}}, inst[31], inst[19:12], inst[20], inst[30:21]};
             end
 	    // sign extended extraction of immediate bits
 	endcase
     end
-            
+           
 endmodule
 
