b29924bf75b9820c33e90100dd2da827bec7865c
Ishita Shankar <anonymousinduvidual1390@gmail.com>
2026-03-13 00:37:44 +0530

    Add files via upload

--- Vector extension/vector_pkg.sv
+++ Vector extension/vector_pkg.sv
@@ -8,20 +8,25 @@ parameter int MAX_VREG = 32;
 typedef logic [VLEN-1:0] vreg_t;
 typedef logic [SEW-1:0] elem_t;
 
-    typedef enum logic [3:0] {
-        VADD,
-        VSUB,
-        VAND,
-        VOR,
-        VXOR,
-        VSLL,   
-        VSRL,
-        VSRA,
-        VMIN,
-        VMAX,
-        VMINU,
-        VMAXU,
-        VLOAD,
-        VSTORE
-    } vector_opcode_t;
+typedef enum logic [3:0] {
+    VADD,
+    VSUB,
+    VAND,
+    VOR,
+    VXOR,
+    VSLL,
+    VSRL,
+    VSRA,
+    VMIN,
+    VMAX,
+    VMINU,
+    VMAXU
+} vec_opcode_t;
+
+typedef enum logic [1:0] {
+    UNIT_STRIDE,
+    STRIDE,
+    INDEX
+} vector_mem_mode_t;
+
 endpackage
