84ea97513596fbe5bcdec5feafd84cb97ad54043
Ishita Shankar <anonymousinduvidual1390@gmail.com>
2026-03-13 00:32:42 +0530

    Add files via upload

---
+++ Vector extension/vector_config.sv
@@ -0,0 +1,51 @@
+module vector_config
+import vector_pkg::*;
+(
+    input  logic clk,
+    input  logic rst,
+    input  logic cfg_write,
+    input  logic [31:0] cfg_data,
+    input  logic cfg_sel_vl,
+    input  logic cfg_sel_sew,
+    output logic [31:0] vl,
+    output logic [31:0] sew,
+    output logic [31:0] epv,
+    output logic [LANES-1:0] lane_active
+);
+
+
+always_ff @(posedge clk or posedge rst) begin     //register configuration
+    if (rst) begin
+        vl  <= VLEN / SEW;
+        sew <= SEW;
+    end
+    else if (cfg_write) begin
+
+        if (cfg_sel_vl)
+            vl <= cfg_data;
+
+        if (cfg_sel_sew)
+            sew <= cfg_data;
+
+    end
+end
+
+assign epv = VLEN / sew;         // elements per vector
+
+integer i;
+
+always_comb begin               //lane control
+
+    for(i=0;i<LANES;i++) begin
+
+        if(i < vl)
+            lane_active[i] = 1;
+
+        else
+            lane_active[i] = 0;
+
+    end
+
+end
+
+endmodule
--- Vector extension/vector_pkg.sv
+++ Vector extension/vector_pkg.sv
@@ -1,23 +1,32 @@
 package vector_pkg;
-    localparam int VLEN     = 256;
-    localparam int SEW      = 32;
-    localparam int LANES    = 8; 
-    localparam int MAX_VREG = 32;
 
-    typedef enum logic [3:0] {
-        VADD,
-        VSUB,
-        VAND,
-        VOR,
-        VXOR,
-        VSLL,   
-        VSRL,
-        VSRA,
-        VMIN,
-        VMAX,
-        VMINU,
-        VMAXU,
-        VLOAD,
-        VSTORE
-    } vector_opcode_t;
+parameter int VLEN  = 256;
+parameter int SEW  = 32;
+parameter int LANES = 4;
+parameter int MAX_VREG = 32;
+
+typedef logic [VLEN-1:0] vreg_t;
+typedef logic [SEW-1:0] elem_t;
+
+typedef enum logic [3:0] {
+    VADD,
+    VSUB,
+    VAND,
+    VOR,
+    VXOR,
+    VSLL,
+    VSRL,
+    VSRA,
+    VMIN,
+    VMAX,
+    VMINU,
+    VMAXU
+} vec_opcode_t;
+
+typedef enum logic [1:0] {
+    UNIT_STRIDE,
+    STRIDE,
+    INDEX
+} vector_mem_mode_t;
+
 endpackage
